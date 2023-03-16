82bcc76d70153d4f14b97d4b75173e458f590ceb
Bessy Aguilar <bessy.aguilar@netcentric.biz>
2023-03-16 16:53:48 +0100

    Test: Minor fix

--- script/app.js
+++ script/app.js
@@ -1,3 +1,4 @@
+const CONTENT_BREAKING_NEWS = "BREAKING NEWS: 5 people arrested for non designing responsively ***** Sprite popularity drops even further after new, less sugary, drink introduction.*****"
 const CONTENT_ARTICLE_TEASERS = [
   "This is an article about first party that happened this weekend",
   "This is an article about second party that happened this weekend",
@@ -5,6 +6,10 @@ const CONTENT_ARTICLE_TEASERS = [
 
 const dynamicContent = () => {
   const teasers = document.querySelectorAll('.teaser__text');
+  const marqueeBar = document.querySelector('marquee');
+
+  // mock content delay
+  setTimeout(() => marqueeBar.innerHTML = CONTENT_BREAKING_NEWS, 3000);
 
   // create dynamic content
   teasers.forEach((teaser, index) => {
